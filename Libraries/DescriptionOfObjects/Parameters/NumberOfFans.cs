244188bb845b48b2cc978982db2eeb5bc3a13db1
koshtoian <koshtoian@tehnogr.ru>
2024-05-16 11:36:54 +0300

    Добавил класс с количеством вентиляторов, обеспечивающих заданную рабочую точку

---
+++ Libraries/DescriptionOfObjects/Parameters/NumberOfFans.cs
@@ -0,0 +1,24 @@
+﻿namespace Libraries.DescriptionOfObjects.Parameters;
+
+public static class NumberOfFans
+{
+    /// <summary>
+    /// Количество вентиляторов, обеспечивающих рабочую точку: короткое название
+    /// </summary>
+    public static readonly int[] Values =
+    {
+        1,
+        2,
+        3
+    };
+
+    /// <summary>
+    ///     Количество вентиляторов, обеспечивающих рабочую точку: полное название
+    /// </summary>
+    public static readonly string[] Names =
+    {
+        "1 = один вентилятор обеспечивают рабочую точку",
+        "2 = два вентилятора обеспечивают рабочую точку",
+        "3 = три вентилятора обеспечивают рабочую точку"
+    };
+}
